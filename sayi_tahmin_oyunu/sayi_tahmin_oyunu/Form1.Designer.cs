91bea783019656186f96eb6cd20e20161f9f5a97
Pelin Pınarbaşı <pelinnpinarbasi@gmail.com>
2026-01-21 00:19:29 +0300

    Sayı tahmin oyunu

---
+++ sayi_tahmin_oyunu/sayi_tahmin_oyunu/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace sayi_tahmin_oyunu
+{
+	partial class Form1
+	{
+		/// <summary>
+		///Gerekli tasarımcı değişkeni.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///Kullanılan tüm kaynakları temizleyin.
+		/// </summary>
+		///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer üretilen kod
+
+		/// <summary>
+		/// Tasarımcı desteği için gerekli metot - bu metodun 
+		///içeriğini kod düzenleyici ile değiştirmeyin.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(287, 96);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(212, 16);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "0 ile 10 arasında tutuğum sayıyı bul";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(316, 235);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(105, 16);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Kalan hakkınız: 3";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(316, 265);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(138, 16);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "sayıya ne kadar yakın";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(333, 144);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 22);
+			this.textBox1.TabIndex = 3;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(345, 183);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "kontrol";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "sayı ne kadar yakın";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+	}
+}
+
---
+++ sayi_tahmin_oyunu/sayi_tahmin_oyunu/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayi_tahmin_oyunu
+{
+   
+	public partial class Form1 : Form
+	{
+		private int tutulanSayi;
+		private int tahminHakki = 3;
+
+		public Form1()
+		{
+			InitializeComponent();
+			YeniSayiTut();
+		}
+
+		private void YeniSayiTut()
+		{
+			Random rnd = new Random();
+			tutulanSayi = rnd.Next(0, 11); 
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (tahminHakki > 0)
+			{
+				int kullaniciTahmini;
+				bool gecerliMi = int.TryParse(textBox1.Text, out kullaniciTahmini);
+
+
+				tahminHakki--;
+
+				if (kullaniciTahmini == tutulanSayi)
+				{
+					label3.Text = "Tebrikler! Doğru tahmin ettiniz.";
+					button1.Enabled = false;
+				}
+				else
+				{
+					if (tahminHakki > 0)
+					{
+						string yonlendirme = kullaniciTahmini < tutulanSayi
+							? "Daha büyük bir sayı girin."
+							: "Daha küçük bir sayı girin.";
+
+						label2.Text = $"Kalan hakkınız: { tahminHakki}";
+						;
+						label3.Text = $"Yanlış! {yonlendirme}";
+					}
+					else
+					{
+						label2.Text = $" Doğru sayı: {tutulanSayi}";
+						label3.Text = $"Üzgünüm, tahmin hakkınız bitti.";
+						button1.Enabled = false;
+					}
+				}
+			}
+		}
+	}
+}u
+
